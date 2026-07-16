7186f8fdc4459432827240780ff3cc9fca76bff1
Emmanuel Ochem <97101294+emmanuelochem@users.noreply.github.com>
2026-07-16 19:25:25 +0100

    Clean up staging URL comments in network_config.dart
    Removed commented-out staging URL from network_config.dart

--- lib/core/network/network_config.dart
+++ lib/core/network/network_config.dart
@@ -3,10 +3,6 @@ bool isLive = false;
 
 //Live url here
 String liveUrl = '';
-
-//Staging base url here
-// String stagingUrl =
-//     'http://ec2-13-41-197-98.eu-west-2.compute.amazonaws.com/api/v1/';
 String stagingUrl = '';
 
 class NetworkConfig {
